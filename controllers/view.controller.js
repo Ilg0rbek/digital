ecdccdeb84e9be0c82927349c106b35b883e5d8b
ilg0rbek <ilgorbekabdumalikov331@gmail.com>
2025-06-08 11:17:26 +0500

    start word

--- controllers/view.controller.js
+++ controllers/view.controller.js
@@ -4,6 +4,7 @@ async function mainPage(req, res) {
   try {
     res.render("pages/home.ejs", {
       title: "Asosiy",
+      path: '/'
     });
   } catch (error) {
     console.log(error.message);
@@ -14,6 +15,7 @@ async function technoPage(req, res) {
   try {
     res.render("pages/techno.ejs", {
       title: "Texnologiya",
+      path: '/technology'
     });
   } catch (error) {
     console.log(error.message);
@@ -24,6 +26,7 @@ async function digitalPage(req, res) {
   try {
     res.render("pages/digital.ejs", {
       title: "Raqamli metodika",
+      path: '/digital-method'
     });
   } catch (error) {
     console.log(error.message);
@@ -34,6 +37,7 @@ async function profPage(req, res) {
   try {
     res.render("pages/prof.ejs", {
       title: "Kasbiy rivojlanish",
+      path: '/professional'
     });
   } catch (error) {
     console.log(error.message);
@@ -44,6 +48,7 @@ async function diagnosPage(req, res) {
   try {
     res.render("pages/diagnos.ejs", {
       title: "Ijodiy diagnostika",
+      path: '/diagnos'
     });
   } catch (error) {
     console.log(error.message);
@@ -54,6 +59,7 @@ async function projectPage(req, res) {
   try {
     res.render("pages/project.ejs", {
       title: "Ijodiy loyihalar",
+      path: '/projects'
     });
   } catch (error) {
     console.log(error.message);
@@ -75,7 +81,8 @@ async function testSolutionPage(req, res) {
         testTitle: selectedTest.title,
         testDescription: selectedTest.description,
         questions: selectedTest.questions,
-        testType: type
+        testType: type,
+        path: '/test-solution'
       });
   } catch (error) {
     console.log(error.message);
@@ -86,6 +93,7 @@ async function loginPage(req, res) {
   try {
     res.render("pages/login.ejs", {
       title: "Kirish",
+      path: '/login'
     });
   } catch (error) {
     console.log(error.message);
@@ -96,6 +104,7 @@ async function registerPage(req, res) {
   try {
     res.render("pages/register.ejs", {
       title: "Ro'yxatdan o'tish",
+      path: '/register'
     });
   } catch (error) {
     console.log(error.message);
@@ -105,10 +114,11 @@ async function registerPage(req, res) {
 async function submitProject(req, res) {
   try {
     res.render("pages/project-submit.ejs", {
-      title: "Loyihani yuklash"
+      title: "Loyihani yuklash",
+      path: '/project-upload'
     })
   } catch (error) {
-    
+    console.log(error.message);
   }
 }
 
